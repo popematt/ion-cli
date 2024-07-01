341625a3ce396077133e43cb8e96cf6336272112
Matthew Pope <popematt@amazon.com>
2024-07-01 16:02:14 -0700

    Remove beta namespace

--- src/bin/ion/commands/generate/templates/mod.rs
+++ src/bin/ion/commands/generate/templates/mod.rs
@@ -2,58 +2,74 @@
 // Currently, there is no other way to add resources like `.templ` files in cargo binary crate.
 // Using these constants allows the binary to access templates through these constants.
 
+const TEMPLATE_BASE_PATH: &str = "/src/bin/ion/commands/generate/templates";
+
 /// Represents java template constants
 pub(crate) mod java {
+    use super::TEMPLATE_BASE_PATH;
     pub(crate) const CLASS: &str = include_str!(concat!(
         env!("CARGO_MANIFEST_DIR"),
-        "/src/bin/ion/commands/beta/generate/templates/java/class.templ"
+        TEMPLATE_BASE_PATH,
+        "java/class.templ"
     ));
     pub(crate) const SCALAR: &str = include_str!(concat!(
         env!("CARGO_MANIFEST_DIR"),
-        "/src/bin/ion/commands/beta/generate/templates/java/scalar.templ"
+        TEMPLATE_BASE_PATH,
+        "java/scalar.templ"
     ));
     pub(crate) const SEQUENCE: &str = include_str!(concat!(
         env!("CARGO_MANIFEST_DIR"),
-        "/src/bin/ion/commands/beta/generate/templates/java/sequence.templ"
+        TEMPLATE_BASE_PATH,
+        "/java/sequence.templ"
     ));
     pub(crate) const UTIL_MACROS: &str = include_str!(concat!(
         env!("CARGO_MANIFEST_DIR"),
-        "/src/bin/ion/commands/beta/generate/templates/java/util_macros.templ"
+        TEMPLATE_BASE_PATH,
+        "/java/util_macros.templ"
     ));
     pub(crate) const NESTED_TYPE: &str = include_str!(concat!(
         env!("CARGO_MANIFEST_DIR"),
-        "/src/bin/ion/commands/beta/generate/templates/java/nested_type.templ"
+        TEMPLATE_BASE_PATH,
+        "/java/nested_type.templ"
     ));
 }
 
 /// Represents rust template constants
 pub(crate) mod rust {
+    use super::TEMPLATE_BASE_PATH;
     pub(crate) const STRUCT: &str = include_str!(concat!(
         env!("CARGO_MANIFEST_DIR"),
-        "/src/bin/ion/commands/beta/generate/templates/rust/struct.templ"
+        TEMPLATE_BASE_PATH,
+        "/rust/struct.templ"
     ));
     pub(crate) const SCALAR: &str = include_str!(concat!(
         env!("CARGO_MANIFEST_DIR"),
-        "/src/bin/ion/commands/beta/generate/templates/rust/scalar.templ"
+        TEMPLATE_BASE_PATH,
+        "/rust/scalar.templ"
     ));
     pub(crate) const SEQUENCE: &str = include_str!(concat!(
         env!("CARGO_MANIFEST_DIR"),
-        "/src/bin/ion/commands/beta/generate/templates/rust/sequence.templ"
+        TEMPLATE_BASE_PATH,
+        "/rust/sequence.templ"
     ));
     pub(crate) const UTIL_MACROS: &str = include_str!(concat!(
         env!("CARGO_MANIFEST_DIR"),
-        "/src/bin/ion/commands/beta/generate/templates/rust/util_macros.templ"
+        TEMPLATE_BASE_PATH,
+        "/rust/util_macros.templ"
     ));
     pub(crate) const RESULT: &str = include_str!(concat!(
         env!("CARGO_MANIFEST_DIR"),
-        "/src/bin/ion/commands/beta/generate/templates/rust/result.templ"
+        TEMPLATE_BASE_PATH,
+        "/rust/result.templ"
     ));
     pub(crate) const NESTED_TYPE: &str = include_str!(concat!(
         env!("CARGO_MANIFEST_DIR"),
-        "/src/bin/ion/commands/beta/generate/templates/rust/nested_type.templ"
+        TEMPLATE_BASE_PATH,
+        "/rust/nested_type.templ"
     ));
     pub(crate) const IMPORT: &str = include_str!(concat!(
         env!("CARGO_MANIFEST_DIR"),
-        "/src/bin/ion/commands/beta/generate/templates/rust/import.templ"
+        TEMPLATE_BASE_PATH,
+        "/rust/import.templ"
     ));
 }
